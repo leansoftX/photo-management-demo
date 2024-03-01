0c25dbab7bbe6139c6ac4d46dde49bcda39f1b4b
adsservice <adsservice@qq.com>
2024-03-01 19:52:56 +0800

    Add file upload functionality to PhotoController

--- src/main/java/com/example/demo/PhotoController.java
+++ src/main/java/com/example/demo/PhotoController.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -36,6 +37,22 @@ public class PhotoController {
     public void deletePhoto(@PathVariable String id) {
         photoRepos.remove(id);
     }
+
+    @PostMapping("/upload")
+    public String uploadPhoto(@RequestParam("file") MultipartFile file) {
+        try {
+            String id = UUID.randomUUID().toString(); // 生成一个随机ID
+            Photo photo = new Photo(id, file.getOriginalFilename()); // 创建新的Photo对象
+            photoRepos.put(id, photo); // 将新的Photo对象添加到map中
+
+            return "You successfully uploaded " + file.getOriginalFilename() + "!";
+        } 
+        catch (Exception e) {
+            return "Fail to upload " + file.getOriginalFilename() + "!";
+        }
+
+
+
 
 
 
